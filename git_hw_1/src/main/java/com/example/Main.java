51f59a9433d0092cae5f16c5db2988ec44b5263e
Katy <bev08021982@gmail.com>
2023-11-28 22:43:28 +0400

    Revert "Добавила стороку Hello java"
    This reverts commit 8684f42ca2638582b1444cba77143d312ad8624c.

--- git_hw_1/src/main/java/com/example/Main.java
+++ git_hw_1/src/main/java/com/example/Main.java
@@ -5,7 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello GIT!");
         System.out.println("Hello GITHUB!");
-        System.out.println("Hello java!");
-
+      
     }
 }
