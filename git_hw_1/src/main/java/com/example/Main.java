49853b4bc0ca7621348543236583d92b2523c5ed
Katy <bev08021982@gmail.com>
2023-11-28 22:45:14 +0400

    Revert "Добавила сторку Hello GIT"
    This reverts commit e0f19bd265d58643a8e042247ade95ba84324cc1.

--- git_hw_1/src/main/java/com/example/Main.java
+++ git_hw_1/src/main/java/com/example/Main.java
@@ -3,8 +3,11 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+<<<<<<< HEAD
         System.out.println("Hello GIT!");
         System.out.println("Hello GITHUB!");
       
+=======
+>>>>>>> parent of e0f19bd (Добавила сторку Hello GIT)
     }
 }
