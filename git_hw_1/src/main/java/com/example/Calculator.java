acd957fc067a8f0ec77d9702bcfa8133d8df0722
Katy <bev08021982@gmail.com>
2023-12-03 21:07:31 +0400

    Добавила класс для разработки

---
+++ git_hw_1/src/main/java/com/example/Calculator.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class Calculator {
+
+    
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0){
+            throw new IllegalArgumentException("Деление на ноль запрещено!");
+        }
+        return a / b;
+    }
+
+    public double calculateDiscount(double a, int b){
+         if (a <= 0 | b <= 0){
+            throw new IllegalArgumentException("Цена или скидка не может быть меньше или равна нулю!");
+        }
+        return (a - (b*(a/100)));
+    }
+
+
+}
+
